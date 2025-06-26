efbbb684cb6f27895b0d31bd5445846fa85aec94
Dharm Thakkar <dharm.codes@gmail.com>
2025-06-26 15:07:12 +0530

    task-3 added

---
+++ Task-3/backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+
+// INFO: Create express app
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// INFO: Middleware
+app.use(express.json());
+app.use(cors());
+
+// INFO: API endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+
+// INFO: Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// INFO: Start server
+app.listen(port, () =>
+  console.log(`Server is running on at http://localhost:${port}`)
+);
